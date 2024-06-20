6cf2d8a5b7eb95d470208106f5ac27009ff85c69
Surajit80kotai <surajit80kotai@gmail.com>
2024-06-20 16:05:26 +0530

    model

--- app/Models/Device_list.php
+++ app/Models/Device_list.php
@@ -8,13 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Device_list extends Model
 {
     use HasFactory;
-    protected $fillable=[
-        'location_name',
-        'street_name',
-        'camera_type',
-        'mac_id',
-        'installation_date',
-        'expire_date',
-        // 'online_status'
+    protected $fillable = [
+        'location_name', 'street_name', 'camera_type', 'mac_id', 'installation_date', 'expire_date', 'online_status'
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($model) {
+            // Generate a custom device_id based on the auto-incrementing id
+            $model->device_id = 'DCX' . str_pad($model->id, 3, '0', STR_PAD_LEFT);
+            $model->save();
+        });
+    }
 }
