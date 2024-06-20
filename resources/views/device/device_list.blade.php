14f39dcfb7270c2dbb4054a797e12c88a4f49e95
Surajit80kotai <surajit80kotai@gmail.com>
2024-06-20 13:31:46 +0530

    add button

--- resources/views/device/device_list.blade.php
+++ resources/views/device/device_list.blade.php
@@ -9,14 +9,16 @@
                 <div class="row">
                     <div class="col-12 col-xl-12 mb-4 mb-xl-0">
                         <div class="justify-content-end d-flex">
-                            <button type="button" class="btn btn-primary btn-icon-text">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                    class="bi bi-plus" viewBox="0 0 16 16">
-                                    <path
-                                        d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
-                                </svg>
-                                Add Device
-                            </button>
+                            <a href="{{ route('create_device_list') }}">
+                                <button type="button" class="btn btn-primary btn-icon-text">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-plus" viewBox="0 0 16 16">
+                                        <path
+                                            d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+                                    </svg>
+                                    Add Device
+                                </button>
+                            </a>
                         </div>
                     </div>
 
@@ -24,49 +26,46 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-12 grid-margin">
-                <div class="row">
-                    <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-                        <!-- Additional content can be added here -->
-                    </div>
-                    <div class="col-12 col-xl-4">
-                        <div class="justify-content-end d-flex">
-                            <div class="dropdown flex-md-grow-1 flex-xl-grow-0">
-                                <button class="btn btn-sm btn-light bg-white dropdown-toggle" type="button"
-                                    id="dropdownMenuDate2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                                    <i class="mdi mdi-calendar"></i> Today (10 Jan 2021)
-                                </button>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuDate2">
-                                    <a class="dropdown-item" href="#">January - March</a>
-                                    <a class="dropdown-item" href="#">March - June</a>
-                                    <a class="dropdown-item" href="#">June - August</a>
-                                    <a class="dropdown-item" href="#">August - November</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
+
             <div class="col-md-12 grid-margin transparent">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">Device List</h4>
-                        <div class="table-responsive">
-                            <table class="table table-hover">
-                                <thead>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Location Name</th>
-                                        <th>Stretch Name</th>
-                                        <th>Camera Type</th>
-                                        <th>MAC ID</th>
-                                        <th>Installation Date</th>
-                                        <th>Expire Date</th>
-                                        <th>Action</th>
-                                    </tr>
-                                </thead>
+              <div class="card">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-12 col-xl-12 mb-3">
+                      <div class="justify-content-end d-flex">
+
+                       <div class="dropdown flex-md-grow-1 flex-xl-grow-0">
+                         <button class="btn btn-sm btn-light bg-white dropdown-toggle" type="button" id="dropdownMenuDate2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                          <i class="mdi mdi-calendar"></i> Today (10 Jan 2021)
+                         </button>
+                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuDate2">
+                           <a class="dropdown-item" href="#">January - March</a>
+                           <a class="dropdown-item" href="#">March - June</a>
+                           <a class="dropdown-item" href="#">June - August</a>
+                           <a class="dropdown-item" href="#">August - November</a>
+                         </div>
+                       </div>
+                      </div>
+                     </div>
+                  </div>
+
+                  <h4 class="card-title">Device List</h4>
+
+                  <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                            <th>Location Name</th>
+                            <th>Stretch Name</th>
+                            <th>Camera Type</th>
+                            <th>MAC ID</th>
+                            <th>Online Status</th>
+                            <th>Installation Date</th>
+                            <th>Expire Date</th>
+
+                            <th>Action</th>
+                            </tr>
+                        </thead>
                                 <tbody>
                                     @php
                                         $sl = 1;
